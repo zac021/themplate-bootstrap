ab5f0685c7a58ea49584da17d6b566f64b5da76b
zack! <zaczac021021@gmail.com>
2022-07-26 06:37:01 -0700

    remove animation

--- emdad.js
+++
@@ -1,6 +0,0 @@
-var sec1=document.getElementById("sec1");
-var show=document.getElementById("show");
-sec1.onmousemove=function(){
-    show.style.color="rgb(33, 104, 165) "
-show.classList.add("move");
-}
